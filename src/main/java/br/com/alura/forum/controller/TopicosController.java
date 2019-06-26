c6b80514b8fc53bb440691734d0adf84e87b5a22
felipe <felipegadelha90@gmail.com>
2019-06-26 19:30:49 -0300

    validações com Bean Validation

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,6 +3,8 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +30,7 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
-	
 	@GetMapping   	//@RequestMapping(method = RequestMethod.GET, value = "/topicos")
-	
 	public List<TopicoDto> lista(String nomeCurso){
 		if(nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
@@ -42,7 +42,7 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form, UriComponentsBuilder uriBuilder) {
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
 		
