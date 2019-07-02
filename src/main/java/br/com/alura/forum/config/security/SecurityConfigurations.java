2963c12537d7e8b872d232ec592174bb38f966bf
felipe <felipegadelha90@gmail.com>
2019-07-01 23:18:33 -0300

    Monitoramento com Actuator e admin e documentação da API com swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -58,6 +58,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
+		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 		.anyRequest().authenticated()
 		.and().csrf().disable()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -68,10 +69,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		
 	}
 	
-	//Configurações de recursos estaticos(js, css, imagens, etc.)
+	//Configurações de recursos estáticos(js, css, imagens, etc.)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
 	}
 	
 //	public static void main(String[] args) {
